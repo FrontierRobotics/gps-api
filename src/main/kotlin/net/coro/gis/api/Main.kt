22dd82c50a4c49c35b77a4145377349b1822e4a6
Andy Condon <condon.andy@gmail.com>
2016-02-06 21:24:58 -0700

    Reading the GPS over the Serial port now! Still need to split it up correctly.

--- src/main/kotlin/net/coro/gis/api/Main.kt
+++ src/main/kotlin/net/coro/gis/api/Main.kt
@@ -1,5 +1,33 @@
 package net.coro.gis.api
 
+import com.fazecast.jSerialComm.SerialPort
+
 fun main(args: Array<String>) {
+
     println("Hello, World!")
+
+    val comPort = SerialPort.getCommPort("/dev/ttyAMA0")
+    comPort.openPort()
+
+    println("Baud Rate: " + comPort.getBaudRate())
+
+    try {
+        while (true) {
+            while (comPort.bytesAvailable() == 0) {
+                Thread.sleep(20);
+            }
+
+            val bytesAvailable : Int = comPort.bytesAvailable()
+
+            println("Bytes Available: " + bytesAvailable)
+
+            val readBuffer: ByteArray = ByteArray(bytesAvailable)
+            val numRead = comPort.readBytes(readBuffer, bytesAvailable.toLong())
+            println("Read " + numRead + " bytes: " + String(readBuffer))
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+
+    comPort.closePort()
 }
