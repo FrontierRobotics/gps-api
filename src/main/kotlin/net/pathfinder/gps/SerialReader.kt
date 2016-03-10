9b317ceb7408dd2941a9afb4dc1d5ec419685710
Andy Condon <andrew.condon@echostar.com>
2016-03-09 21:14:24 -0700

    Implemented `Closeable` and encapsulated the serial library.

--- src/main/kotlin/net/pathfinder/gps/SerialReader.kt
+++ src/main/kotlin/net/pathfinder/gps/SerialReader.kt
@@ -1,15 +1,26 @@
 package net.pathfinder.gps
 
 import com.fazecast.jSerialComm.SerialPort
+import java.io.Closeable
 
-class SerialReader(val serialPort: SerialPort) {
+class SerialReader(portDescripter: String) : Closeable {
+
+    val serialPort = SerialPort.getCommPort(portDescripter)
+
+    init {
+        serialPort.openPort()
+    }
+
+    override fun close() {
+        serialPort.closePort()
+    }
 
     fun readSentence(): String {
         while (serialPort.bytesAvailable() == 0) {
             Thread.sleep(20);
         }
 
-        val bytesAvailable : Int = serialPort.bytesAvailable()
+        val bytesAvailable: Int = serialPort.bytesAvailable()
 
         println("Bytes Available: " + bytesAvailable)
 
@@ -18,5 +29,9 @@ class SerialReader(val serialPort: SerialPort) {
 
 
         return String(readBuffer)
+    }
+
+    override fun toString(): String {
+        return "${serialPort.baudRate} baud"
     }
 }
--- src/main/kotlin/net/pathfinder/gps/api/Main.kt
+++ src/main/kotlin/net/pathfinder/gps/api/Main.kt
@@ -1,26 +1,14 @@
 package net.pathfinder.gps.api
 
-import com.fazecast.jSerialComm.SerialPort
 import net.pathfinder.gps.SerialReader
 
 fun main(args: Array<String>) {
 
     println("Hello, World!")
 
-    val comPort = SerialPort.getCommPort("/dev/ttyAMA0")
-    comPort.openPort()
-
-    val nmeaReader = SerialReader(comPort)
-
-    println("Baud Rate: " + comPort.baudRate)
-
-    try {
+    SerialReader("/dev/ttyAMA0").use {
         while (true) {
-            println("NMEA: " + nmeaReader.readSentence())
+            println("NMEA: " + it.readSentence())
         }
-    } catch (e: Exception) {
-        e.printStackTrace()
     }
-
-    comPort.closePort()
 }
