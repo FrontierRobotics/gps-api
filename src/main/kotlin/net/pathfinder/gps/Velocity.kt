c7f3d7f50a26d81a9bd19f7f7c93303c76171a94
Andy Condon <andrew.condon@echostar.com>
2016-03-11 18:31:35 -0700

    Base unit should be meters per second.

--- src/main/kotlin/net/pathfinder/gps/Velocity.kt
+++ src/main/kotlin/net/pathfinder/gps/Velocity.kt
@@ -1,19 +1,23 @@
 package net.pathfinder.gps
 
 
-class Velocity(kilometersPerHour: Double) {
+class Velocity private constructor (val metersPerSecond: Double) {
 
     companion object {
         fun fromKnots(knots: Double): Velocity {
-            return Velocity(knots * 1.852) //exact
+            return fromKilometerPerHour(knots * 1.852) //exact
         }
 
         fun fromMilesPerHour(mph: Double): Velocity {
-            return Velocity(mph * 1.609344) //exact
+            return fromKilometerPerHour(mph * 1.609344) //exact
         }
 
         fun fromMetersPerSecond(mps: Double): Velocity {
-            return Velocity(mps * 3.6) //exact
+            return Velocity(mps) //exact
+        }
+
+        fun fromKilometerPerHour(kph: Double): Velocity {
+            return Velocity(kph * 0.277778)
         }
     }
 }
