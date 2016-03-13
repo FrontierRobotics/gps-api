c107e96c467914ed16e34ff9e6708af43b6b6934
Andy Condon <andrew.condon@echostar.com>
2016-03-13 16:10:02 -0600

    Little cleanup.

--- src/main/kotlin/net/pathfinder/gps/Distance.kt
+++ src/main/kotlin/net/pathfinder/gps/Distance.kt
@@ -1,8 +1,9 @@
 package net.pathfinder.gps
 
 class Distance private constructor(val meters: Double) {
+
     companion object {
-        fun ofMeters(meters: Double): Distance = Distance(meters)
+        fun ofMeters(meters: Double) = Distance(meters)
     }
 
     fun toMeters() = meters
--- src/main/kotlin/net/pathfinder/gps/Speed.kt
+++ src/main/kotlin/net/pathfinder/gps/Speed.kt
@@ -6,20 +6,20 @@ import java.time.Duration
 class Speed (val distance: Distance, val duration: Duration) {
 
     companion object {
-        fun ofMetersPerSecond(mps: Double): Speed = Speed(Distance.ofMeters(mps), Duration.ofSeconds(1))
+        fun ofMetersPerSecond(mps: Double) = Speed(Distance.ofMeters(mps), Duration.ofSeconds(1))
 
-        fun ofKilometersPerHour(kph: Double): Speed = ofMetersPerSecond(kph * 0.277778)
+        fun ofKilometersPerHour(kph: Double) = ofMetersPerSecond(kph * 0.277778)
 
-        fun ofMilesPerHour(mph: Double): Speed = ofMetersPerSecond(mph * 0.44704)
+        fun ofMilesPerHour(mph: Double) = ofMetersPerSecond(mph * 0.44704)
 
-        fun ofKnots(knots: Double): Speed = ofKilometersPerHour(knots * 1.852)
+        fun ofKnots(knots: Double) = ofKilometersPerHour(knots * 1.852)
     }
 
-    fun toMetersPerSecond(): Double = distance.toMeters() / duration.seconds
+    fun toMetersPerSecond() = distance.toMeters() / duration.seconds
 
-    fun toKilometersPerHour(): Double = toMetersPerSecond() * 3.6
+    fun toKilometersPerHour() = toMetersPerSecond() * 3.6
 
-    fun toMilesPerHour(): Double = toMetersPerSecond() * 2.236936
+    fun toMilesPerHour() = toMetersPerSecond() * 2.236936
 
-    fun toKnots(): Double = toMetersPerSecond() * 1.943844
+    fun toKnots() = toMetersPerSecond() * 1.943844
 }
